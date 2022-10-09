5bd60615e59da6af7e260a7da63215603cf34741
albertops02 <albertoperez021405@gmail.com>
2022-10-09 13:37:19 +0200

    Reducido Alberto

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import time
-
+import docker
 import infraestructura
 import os
-
+client = docker.from_env()
 image = 'httpd'
 binding = {80: 8080}
+
+def runcontainerwithport(image, binding):
+    container = client.containers.run(image, detach=True, ports=binding)
+    print("Container started with ID: {}".format(container.id))
+
+def pullimage(imagename):
+    image = client.images.pull(imagename)
+    print(image.id)    
 
 print("Downloading Image...")
 infraestructura.pullimage(image)
